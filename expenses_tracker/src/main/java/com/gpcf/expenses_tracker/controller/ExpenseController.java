0210f053ea193c98b61587d2f0ce70d5656b3bb4
Gaurav-pratap-CodeFusion <gauravpratap144@gmail.com>
2026-05-05 14:23:19 +0530

    Update Backend

--- expenses_tracker/src/main/java/com/gpcf/expenses_tracker/controller/ExpenseController.java
+++ expenses_tracker/src/main/java/com/gpcf/expenses_tracker/controller/ExpenseController.java
@@ -3,6 +3,7 @@ package com.gpcf.expenses_tracker.controller;
 import com.gpcf.expenses_tracker.entity.Expense;
 import com.gpcf.expenses_tracker.service.ExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,13 +32,13 @@ public class ExpenseController {
     @PostMapping
     public ResponseEntity<Expense> addExpense(@RequestBody Expense expense) {
         Expense savedExpense = service.saveExpense(expense);
-        return ResponseEntity.ok(savedExpense);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedExpense);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Expense> updateExpense(@PathVariable Long id, @RequestBody Expense expense) {
         Expense updatedExpense = service.updateExpense(id, expense);
-        return ResponseEntity.ok(updatedExpense);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
     @DeleteMapping("/{id}")
